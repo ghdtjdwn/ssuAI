8001dafc49b71725e7ca4133722f5b33bf18fc43
ghdtjdwn <seongjuice999@gmail.com>
2026-05-10 21:50:06 +0900

    chore(backend): update Spring Boot to 4

--- backend/src/test/java/com/ssuai/domain/dorm/controller/DormMealControllerTests.java
+++ backend/src/test/java/com/ssuai/domain/dorm/controller/DormMealControllerTests.java
@@ -13,9 +13,9 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.ssuai.domain.dorm.service.DormMealService;
@@ -31,7 +31,7 @@ class DormMealControllerTests {
 
     private final MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private DormMealService dormMealService;
 
     @Autowired
